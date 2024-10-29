5ca7d55eca9c367f0d4d0ab9dd2d10457d33d495
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-10-29 11:34:13 -0400

    Create createPlaylist.ts

---
+++ src/util/createPlaylist.ts
@@ -0,0 +1,34 @@
+import { currentToken } from "./currentToken";
+
+type PlaylistResponseBody = {
+  name: string;
+  description: string;
+  public: boolean;
+};
+
+export const createPlaylist = (userId: string) => {
+  const spotifyUrl = `https://api.spotify.com/v1/users/${userId}/playlists`;
+
+  return fetch(spotifyUrl, {
+    headers: {
+      Authorization: `Bearer ${currentToken.access_token}`,
+    },
+    method: "POST",
+    body: JSON.stringify({
+      name: "My playlist",
+      description: "My description",
+      public: false,
+    }),
+  })
+    .then((response) => {
+      console.log("response", response);
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      console.log("successfully created playlist");
+      console.log("jsonResponse", jsonResponse);
+    })
+    .catch((error) => {
+      console.error("Error fetching tracks", error);
+    });
+};
